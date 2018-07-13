907a2c8b8333da1966bb982758c3758f510b6cd6
Pierre Sandboge <pierre@sandboge.com>
2018-07-13 17:53:53 +0200

    Extract Swedish entries from jmdict

--- src/main/java/se/sandboge/jmdictutils/Extract.java
+++ src/main/java/se/sandboge/jmdictutils/Extract.java
@@ -15,8 +15,10 @@ import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.sql.*;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -26,20 +28,63 @@ import java.util.regex.Pattern;
  * Use with jmdict (http://edrdg.org/jmdict/j_jmdict.html)
  */
 public class Extract {
-    private static final String GENERIC = "/jtransform.xsl";
+    private static final String JTRANSFORM＿SV = "/jtransform＿sv.xsl";
 
     public static void main(String[] args) {
         String sourcePath = args[0];
-        String transform = GENERIC;
+        String transform = JTRANSFORM＿SV;
 
         if (args.length == 2 && args[1].equals("generic")) {
-            transform = GENERIC;
+            transform = JTRANSFORM＿SV;
         }
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         String text = parseFile(transform, sourcePath, factory);
 
-        System.out.println(text);
+        List<Row> rows = splitText(text);
+        for (Row row :
+                rows) {
+            System.out.println(row);
+        }
+    }
+
+    private static List<Row> splitText(String text) {
+        List<Row> rows = new ArrayList<>();
+        String[] rowStrings = text.split(Pattern.quote("\n"));
+        for (String rowString :
+                rowStrings) {
+            List<Row> row = splitRow(rowString);
+            rows.addAll(row);
+        }
+        return rows;
+    }
+
+    private static List<Row> splitRow(String rowString) {
+        String[] items = rowString.split(Pattern.quote("€"));
+        List<Row> row = new ArrayList<>();
+        if (items.length < 4) {
+            System.out.println("Line err: " + rowString);
+            return row;
+        } else if (items.length > 4) {
+            int index = 0;
+            for(int i = 0; i < 4; i++) {
+                index = rowString.indexOf('€', index) + 1;
+            }
+            index --;
+            System.out.println(rowString + ", " + index);
+            row.addAll(splitRow(rowString.substring(index + 1)));
+        }
+        List<String> en = splitWords(items[3]);
+        List<String> sv = splitWords(items[2]);
+        List<String> writing = splitWords(items[0]);
+        List<String> reading = splitWords(items[1]);
+        row.add(new Row(reading, writing, sv, en));
+        return row;
+    }
+
+    private static List<String> splitWords(String item) {
+        String[] words = item.split(Pattern.quote(","));
+        return Arrays.asList(words);
     }
 
     private static String parseFile(String template, String file, DocumentBuilderFactory factory) {
@@ -63,4 +108,40 @@ public class Extract {
         return null;
     }
 
+    private static class Row {
+        List<String> reading;
+        List<String> writing;
+        List<String> sv;
+        List<String> en;
+
+        public Row(List<String> reading, List<String> writing, List<String> sv, List<String> en) {
+            this.reading = reading;
+            this.writing = writing;
+            this.sv = sv;
+            this.en = en;
+        }
+
+        @Override
+        public String toString() {
+            String result = "w:";
+            result += asString(writing);
+            result += ";r:";
+            result += asString(reading);
+            result += ";s:";
+            result += asString(sv);
+            result += ";e:";
+            result += asString(en);
+            return result;
+        }
+
+    }
+
+    static private String asString(List<String> reading) {
+        StringBuilder sb = new StringBuilder();
+        for (String s :
+                reading) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
 }
---
+++ src/main/resources/jtransform＿sv.xsl
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet
+        xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+        version="1.0"
+>
+    <xsl:strip-space elements="*"/>
+    <xsl:output method="text"/>
+
+    <xsl:template match="entry">
+        <xsl:if test="descendant::gloss[@lang='swe']">
+            <xsl:text>&#xa;</xsl:text>
+        </xsl:if>
+        <xsl:apply-templates select="sense"/>
+    </xsl:template>
+
+    <xsl:template match="sense">
+        <xsl:if test="child::gloss[@lang='swe']">
+            <xsl:apply-templates select="../k_ele"/>
+            <xsl:text>€</xsl:text>
+            <xsl:apply-templates select="../r_ele"/>
+            <xsl:text>€</xsl:text>
+            <xsl:apply-templates select="gloss[@lang='swe']"/>
+            <xsl:text>€</xsl:text>
+            <xsl:apply-templates select="../sense/gloss[@lang='eng']"/>
+            <xsl:text>€</xsl:text>
+        </xsl:if>
+    </xsl:template>
+
+    <xsl:template match="gloss">
+        <xsl:value-of select="text()"/>
+        <xsl:text>,</xsl:text>
+    </xsl:template>
+
+    <xsl:template match="r_ele">
+        <xsl:apply-templates select="reb"/>
+    </xsl:template>
+
+    <xsl:template match="k_ele">
+        <xsl:apply-templates select="keb"/>
+    </xsl:template>
+
+    <xsl:template match="reb">
+        <xsl:value-of select="text()"/>
+        <xsl:text>,</xsl:text>
+    </xsl:template>
+
+    <xsl:template match="keb">
+        <xsl:value-of select="text()"/>
+        <xsl:text>,</xsl:text>
+    </xsl:template>
+
+</xsl:stylesheet>
